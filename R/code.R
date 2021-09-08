b2d757099646335849387cd0768096fe33e2b45d
MACKAY Jill <jmackay4@ed.ac.uk>
2021-09-08 17:14:36 +0100

    more wrds

--- R/code.R
+++ R/code.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 
 dat <- tibble(goal = 20000,
-              written = 10522) %>% 
+              written = 12011) %>% 
   mutate(perc = written/goal,
          unwritten = goal-written) %>% 
   pivot_longer(cols = c(goal:unwritten),
